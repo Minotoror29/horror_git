95e8d9229e9e1228d1bfcdc797639709ae82e0d0
Minotoror29 <matthieu.boissel@laposte.net>
2023-10-17 11:35:40 +0200

    Basic Player Controls

--- Horror/Assets/Scripts/PlayerController.cs
+++ Horror/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rb;
+    private PlayerControls _playerControls;
+
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed = 150f;
+    private Vector2 _movementDirection;
+
+    private void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateLogic();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePhysics();
+    }
+
+    public void Initialize()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        _playerControls = new PlayerControls();
+        _playerControls.InGame.Enable();
+    }
+
+    public void UpdateLogic()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        _movementDirection = _playerControls.InGame.Movement.ReadValue<Vector2>();
+    }
+
+    public void UpdatePhysics()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _rb.velocity = _movementDirection * Time.fixedDeltaTime * movementSpeed;
     }
 }
